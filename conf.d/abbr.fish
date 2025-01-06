fdc701383cf62ce68f29f4000c3153354f015516
Alexandre Trufanow <alexandre.trufanow@tenacy.io>
2025-01-06 11:41:50 +0100

    Update git aliases

--- conf.d/abbr.fish
+++ conf.d/abbr.fish
@@ -4,9 +4,15 @@ abbr less bat
 
 # git
 abbr -a ga 'git add'
-abbr -a gc 'git commit'
-abbr -a gd 'git diff'
-abbr -a gs 'git status'
 abbr -a gco 'git checkout'
+abbr -a gc 'git commit'
 abbr -a gpl 'git pull'
 abbr -a gps 'git push'
+abbr -a gpf 'git push --force'
+abbr -a gs 'git status -sb'
+abbr -a gsta 'git stash'
+abbr -a gstp 'git stash pop'
+abbr -a gd 'git diff'
+abbr -a gm 'git merge'
+abbr -a gb 'git checkout -b'
+abbr -a glb 'git latest-branches'
