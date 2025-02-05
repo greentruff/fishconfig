fe27d483aecb06812cbedf8523f7877637c38d6e
Alexandre Trufanow <alexandre.trufanow@tenacy.io>
2025-02-05 12:50:06 +0100

    Add tenacy-python helper functions

---
+++ conf.d/tenacy.fish
@@ -0,0 +1,19 @@
+
+set BACKEND_PATH ~/dev/tenacy-python/backend
+
+if test -d
+    envsource $BACKEND_PATH/local.env
+    set -gx ROOT_BACKEND_PROJECT $BACKEND_PATH/back
+    set -gx FLASK_APP $BACKEND_PATH/back/src/run.py
+
+    # brew install coreutils
+    function tdb
+        genv -C $ROOT_BACKEND_PROJECT/src poetry run flask db
+    end
+    function tpt
+        genv -C $ROOT_BACKEND_PROJECT/src poetry run pytest
+    end
+    function tpo
+        genv -C $ROOT_BACKEND_PROJECT/src poetry
+    end
+end
