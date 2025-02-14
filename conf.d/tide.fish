f78b608509a077d4b8eda8cd662034d6bdba16a7
Alexandre Trufanow <alexandre.trufanow@tenacy.io>
2025-02-14 14:41:03 +0100

    add tide config

---
+++ conf.d/tide.fish
@@ -0,0 +1,11 @@
+function refresh_tide
+    tide configure  \
+        --auto  \
+        --style=Lean \
+        --prompt_colors='True color' \
+        --show_time=No \
+        --lean_prompt_height='One line' \
+        --prompt_spacing=Compact \
+        --icons='Few icons' \
+        --transient=No
+end
