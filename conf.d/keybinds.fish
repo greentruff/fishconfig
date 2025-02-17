f09e7ee1d257f045421ede67ab060555b14c451b
Alexandre Trufanow <trufanowa@gmail.com>
2025-02-17 08:53:52 +0100

    add readline keybinds for vi insert mode

---
+++ conf.d/keybinds.fish
@@ -0,0 +1,16 @@
+fish_vi_key_bindings
+
+# Additional readline keybinds for vi insert mode
+bind --user -M insert \ca beginning-of-line
+bind --user -M insert \ce end-of-line
+bind --user -M insert \ch backward-delete-char
+bind --user -M insert \cp up-or-search
+bind --user -M insert \cn down-or-search
+bind --user -M insert \cf forward-char
+bind --user -M insert \cb backward-char
+bind --user -M insert \ct transpose-chars
+bind --user -M insert \cg cancel
+bind --user -M insert \cz undo
+bind --user -M insert \cZ redo
+bind --user -M insert \eb prevd-or-backward-word
+bind --user -M insert \ef nextd-or-forward-word
