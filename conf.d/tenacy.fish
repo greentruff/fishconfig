5a6ea03508775aaddc906a3270b08de5cc028618
Alexandre Trufanow <alexandre.trufanow@tenacy.io>
2025-04-11 12:18:37 +0200

    tenacy: warn if docker is not running

--- conf.d/tenacy.fish
+++ conf.d/tenacy.fish
@@ -6,11 +6,14 @@ if test -d $BACKEND_PATH
     set -gx ROOT_BACKEND_PROJECT $BACKEND_PATH/back
     set -gx FLASK_APP $BACKEND_PATH/back/src/run.py
 
-    # brew install coreutils
     function tdb
         genv -C $ROOT_BACKEND_PROJECT/src poetry run flask db $argv
     end
     function tpt
+        if ! docker info > /dev/null 2>&1;
+            echo "Docker is not running"
+            return 1
+        end
         genv -C $ROOT_BACKEND_PROJECT/src poetry run pytest $argv
     end
     function tpo
