0222d20e41dd2cfbda68f148efa6031b0684ce0c
Alexandre Trufanow <trufanowa@gmail.com>
2025-08-25 00:54:41 +0200

    tide: Add fish

--- conf.d/path.fish
+++ conf.d/path.fish
@@ -2,10 +2,11 @@ set tool_paths
 set -a tool_paths $HOME/.cargo/bin
 set -a tool_paths $HOME/go/bin
 set -a tool_paths $HOME/.docker/bin
+set -a tool_paths $HOME/.config/emacs/bin
 
 set home_paths
-set -a home_paths $HOME/.local/bin 
-set -a home_paths $HOME/bin 
+set -a home_paths $HOME/.local/bin
+set -a home_paths $HOME/bin
 
 for p in $tool_paths $home_paths
     if test -d $p
---
+++ functions/_tide_item_jj.fish
@@ -0,0 +1,26 @@
+function _tide_item_jj
+    if not command -sq jj; or not jj root --quiet &>/dev/null
+        return 1
+    end
+
+    set jj_status (jj log -r@ -n1 --no-graph --color always -T '
+    separate("|",
+        coalesce(
+            bookmarks.join(" "),
+            self.parents().filter(|c| !c.immutable() && c.bookmarks().len() > 0).map(|c| c.bookmarks().join(", ")).join("\\n") ++ "≠"
+        ),
+        change_id.shortest(),
+        coalesce(
+            if(
+                description.first_line().substr(0, 24).starts_with(description.first_line()),
+                description.first_line().substr(0, 24),
+                description.first_line().substr(0, 23) ++ "…"
+            ),
+            "no description"
+        ),
+        if(conflict, label("conflict", "(conflict)")),
+        if(empty, label("empty", "(empty)")),
+        if(divergent, "(divergent)"),
+    )' | string trim)
+    _tide_print_item jj $tide_jj_icon' ' "($jj_status)"
+end
