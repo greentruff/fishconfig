3be3365cc1b9042ed09617528c0ea00daf58676f
Alexandre Trufanow <trufanowa@gmail.com>
2026-01-25 16:40:18 +0100

    Add refresh_tide for tide config

--- conf.d/tide.fish
+++ conf.d/tide.fish
@@ -1,11 +1,18 @@
+# run on new install
 function refresh_tide
     tide configure  \
         --auto  \
         --style=Lean \
         --prompt_colors='True color' \
-        --show_time=No \
-        --lean_prompt_height='One line' \
+        --show_time='24-hour format' \
+        # --lean_prompt_height='One line' \
+        --lean_prompt_height='Two lines' \
+        --prompt_connection='Disconnected' \
         --prompt_spacing=Compact \
         --icons='Few icons' \
-        --transient=No
+        --transient=Yes
+
+    set -U tide_left_prompt_items context pwd git newline character
+    set -U tide_right_prompt_items status cmd_duration context jobs direnv kubectl toolbox terraform nix_shell time vi_mode
+    set -U tide_git_truncation_length 50
 end
