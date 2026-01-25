29fc734ae73fe0f7832ac4b1c4fc9038085586d5
Alexandre Trufanow <trufanowa@gmail.com>
2026-01-25 16:40:19 +0100

    prompt: remove duplicate context

--- conf.d/tide.fish
+++ conf.d/tide.fish
@@ -13,6 +13,6 @@ function refresh_tide
         --transient=Yes
 
     set -U tide_left_prompt_items context pwd git newline character
-    set -U tide_right_prompt_items status cmd_duration context jobs direnv kubectl toolbox terraform nix_shell time vi_mode
+    set -U tide_right_prompt_items status cmd_duration jobs direnv kubectl toolbox terraform nix_shell time vi_mode
     set -U tide_git_truncation_length 50
 end
